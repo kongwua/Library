e60ec9b0defd2d6ba313f8f2e0b9894f5e670bc7
kongwua <ljh18702062957@163.com>
2024-11-27 23:47:24 +0800

    基本要求已完成，剩预约、罚款功能以及管理员与用户隐藏等部分细节

---
+++ cmake-build-debug/Library_autogen/include/ui_bookinfo_dialog.h
@@ -0,0 +1,256 @@
+/********************************************************************************
+** Form generated from reading UI file 'bookinfo_dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 6.5.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_BOOKINFO_DIALOG_H
+#define UI_BOOKINFO_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAbstractButton>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QDoubleSpinBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QVBoxLayout>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_bookinfo_dialog
+{
+public:
+    QGridLayout *gridLayout;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_5;
+    QSpacerItem *horizontalSpacer_2;
+    QLabel *num_label;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *delete_btn;
+    QSpacerItem *horizontalSpacer;
+    QDialogButtonBox *buttonBox;
+    QHBoxLayout *horizontalLayout_5;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QSpacerItem *horizontalSpacer_4;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QLineEdit *name_edit;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label_2;
+    QLineEdit *isbn_edit;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_3;
+    QSpinBox *num_spinBox;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_4;
+    QDoubleSpinBox *price_doubleSpinBox;
+    QTableView *reader_tableView;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_7;
+    QSpacerItem *horizontalSpacer_3;
+    QLabel *label_8;
+    QTableView *reserve_tableView;
+
+    void setupUi(QDialog *bookinfo_dialog)
+    {
+        if (bookinfo_dialog->objectName().isEmpty())
+            bookinfo_dialog->setObjectName("bookinfo_dialog");
+        bookinfo_dialog->resize(649, 534);
+        gridLayout = new QGridLayout(bookinfo_dialog);
+        gridLayout->setObjectName("gridLayout");
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        label_5 = new QLabel(bookinfo_dialog);
+        label_5->setObjectName("label_5");
+
+        horizontalLayout_3->addWidget(label_5);
+
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_2);
+
+        num_label = new QLabel(bookinfo_dialog);
+        num_label->setObjectName("num_label");
+
+        horizontalLayout_3->addWidget(num_label);
+
+
+        gridLayout->addLayout(horizontalLayout_3, 2, 0, 1, 1);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        delete_btn = new QPushButton(bookinfo_dialog);
+        delete_btn->setObjectName("delete_btn");
+
+        horizontalLayout_2->addWidget(delete_btn);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer);
+
+        buttonBox = new QDialogButtonBox(bookinfo_dialog);
+        buttonBox->setObjectName("buttonBox");
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+
+        horizontalLayout_2->addWidget(buttonBox);
+
+
+        gridLayout->addLayout(horizontalLayout_2, 1, 0, 1, 1);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName("horizontalLayout_5");
+        pushButton_2 = new QPushButton(bookinfo_dialog);
+        pushButton_2->setObjectName("pushButton_2");
+
+        horizontalLayout_5->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(bookinfo_dialog);
+        pushButton_3->setObjectName("pushButton_3");
+
+        horizontalLayout_5->addWidget(pushButton_3);
+
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_5->addItem(horizontalSpacer_4);
+
+
+        gridLayout->addLayout(horizontalLayout_5, 6, 0, 1, 1);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        label = new QLabel(bookinfo_dialog);
+        label->setObjectName("label");
+
+        verticalLayout->addWidget(label);
+
+        name_edit = new QLineEdit(bookinfo_dialog);
+        name_edit->setObjectName("name_edit");
+
+        verticalLayout->addWidget(name_edit);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        label_2 = new QLabel(bookinfo_dialog);
+        label_2->setObjectName("label_2");
+
+        verticalLayout_2->addWidget(label_2);
+
+        isbn_edit = new QLineEdit(bookinfo_dialog);
+        isbn_edit->setObjectName("isbn_edit");
+
+        verticalLayout_2->addWidget(isbn_edit);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        label_3 = new QLabel(bookinfo_dialog);
+        label_3->setObjectName("label_3");
+
+        verticalLayout_3->addWidget(label_3);
+
+        num_spinBox = new QSpinBox(bookinfo_dialog);
+        num_spinBox->setObjectName("num_spinBox");
+
+        verticalLayout_3->addWidget(num_spinBox);
+
+
+        horizontalLayout->addLayout(verticalLayout_3);
+
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setObjectName("verticalLayout_4");
+        label_4 = new QLabel(bookinfo_dialog);
+        label_4->setObjectName("label_4");
+
+        verticalLayout_4->addWidget(label_4);
+
+        price_doubleSpinBox = new QDoubleSpinBox(bookinfo_dialog);
+        price_doubleSpinBox->setObjectName("price_doubleSpinBox");
+
+        verticalLayout_4->addWidget(price_doubleSpinBox);
+
+
+        horizontalLayout->addLayout(verticalLayout_4);
+
+
+        gridLayout->addLayout(horizontalLayout, 0, 0, 1, 1);
+
+        reader_tableView = new QTableView(bookinfo_dialog);
+        reader_tableView->setObjectName("reader_tableView");
+
+        gridLayout->addWidget(reader_tableView, 3, 0, 1, 1);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        label_7 = new QLabel(bookinfo_dialog);
+        label_7->setObjectName("label_7");
+
+        horizontalLayout_4->addWidget(label_7);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_3);
+
+        label_8 = new QLabel(bookinfo_dialog);
+        label_8->setObjectName("label_8");
+
+        horizontalLayout_4->addWidget(label_8);
+
+
+        gridLayout->addLayout(horizontalLayout_4, 4, 0, 1, 1);
+
+        reserve_tableView = new QTableView(bookinfo_dialog);
+        reserve_tableView->setObjectName("reserve_tableView");
+
+        gridLayout->addWidget(reserve_tableView, 5, 0, 1, 1);
+
+
+        retranslateUi(bookinfo_dialog);
+
+        QMetaObject::connectSlotsByName(bookinfo_dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *bookinfo_dialog)
+    {
+        bookinfo_dialog->setWindowTitle(QCoreApplication::translate("bookinfo_dialog", "\345\233\276\344\271\246\350\257\246\347\273\206\344\277\241\346\201\257", nullptr));
+        label_5->setText(QCoreApplication::translate("bookinfo_dialog", "\345\200\237\351\230\205\346\234\254\344\271\246\347\232\204\347\224\250\346\210\267", nullptr));
+        num_label->setText(QCoreApplication::translate("bookinfo_dialog", "\345\205\261\345\200\237\345\207\272 0 \346\234\254", nullptr));
+        delete_btn->setText(QCoreApplication::translate("bookinfo_dialog", "\345\210\240\351\231\244\345\233\276\344\271\246", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("bookinfo_dialog", "\346\267\273\345\212\240\345\200\237\351\230\205\350\200\205", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("bookinfo_dialog", "\347\247\273\351\231\244\345\200\237\351\230\205\350\200\205", nullptr));
+        label->setText(QCoreApplication::translate("bookinfo_dialog", "\344\271\246\345\220\215", nullptr));
+        label_2->setText(QCoreApplication::translate("bookinfo_dialog", "\344\271\246\345\217\267", nullptr));
+        label_3->setText(QCoreApplication::translate("bookinfo_dialog", "\346\225\260\351\207\217", nullptr));
+        label_4->setText(QCoreApplication::translate("bookinfo_dialog", "\344\273\267\346\240\274", nullptr));
+        label_7->setText(QCoreApplication::translate("bookinfo_dialog", "\351\242\204\347\272\246\346\234\254\344\271\246\347\232\204\347\224\250\346\210\267", nullptr));
+        label_8->setText(QCoreApplication::translate("bookinfo_dialog", "\345\205\261\351\242\204\347\272\246 0 \346\234\254", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class bookinfo_dialog: public Ui_bookinfo_dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_BOOKINFO_DIALOG_H
