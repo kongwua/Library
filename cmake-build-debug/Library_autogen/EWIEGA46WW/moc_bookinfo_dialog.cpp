5cf32b3c199bfcd8c3815d4fe64e1e7f3439af7b
kongwua <ljh18702062957@163.com>
2024-11-27 23:48:27 +0800

    基本要求已完成，剩预约、罚款功能以及管理员与用户隐藏等部分细节

---
+++ cmake-build-debug/Library_autogen/EWIEGA46WW/moc_bookinfo_dialog.cpp
@@ -0,0 +1,156 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'bookinfo_dialog.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../bookinfo_dialog.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'bookinfo_dialog.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS = QtMocHelpers::stringData(
+    "bookinfo_dialog",
+    "on_buttonBox_accepted",
+    "",
+    "on_delete_btn_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS_t {
+    uint offsetsAndSizes[8];
+    char stringdata0[16];
+    char stringdata1[22];
+    char stringdata2[1];
+    char stringdata3[22];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS_t qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 15),  // "bookinfo_dialog"
+        QT_MOC_LITERAL(16, 21),  // "on_buttonBox_accepted"
+        QT_MOC_LITERAL(38, 0),  // ""
+        QT_MOC_LITERAL(39, 21)   // "on_delete_btn_clicked"
+    },
+    "bookinfo_dialog",
+    "on_buttonBox_accepted",
+    "",
+    "on_delete_btn_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSbookinfo_dialogENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x0a,    1 /* Public */,
+       3,    0,   27,    2, 0x0a,    2 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject bookinfo_dialog::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSbookinfo_dialogENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<bookinfo_dialog, std::true_type>,
+        // method 'on_buttonBox_accepted'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_delete_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void bookinfo_dialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<bookinfo_dialog *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_buttonBox_accepted(); break;
+        case 1: _t->on_delete_btn_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *bookinfo_dialog::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *bookinfo_dialog::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSbookinfo_dialogENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int bookinfo_dialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
